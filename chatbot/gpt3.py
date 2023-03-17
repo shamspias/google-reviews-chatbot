3634a805a1ef4f84aaccba787c5199ebc24584cb
shamspias <shamsuddinpias0@gmail.com>
2023-03-17 18:55:10 +0600

    add gpt3 response

---
+++ chatbot/gpt3.py
@@ -0,0 +1,8 @@
+from openai import OpenAI
+from config.settings import OPENAI_API_KEY
+
+
+def generate_response_gpt3(prompt):
+    # Implement your GPT-3 API call here
+    # Use the OPENAI_API_KEY from the config.settings module
+    pass
