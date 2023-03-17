79dc55d97142201b2b10dd1abaa43ddea7031171
shamspias <shamsuddinpias0@gmail.com>
2023-03-17 19:01:06 +0600

    fix: gpt3

--- chatbot/gpt3.py
+++ chatbot/gpt3.py
@@ -1,8 +1,32 @@
-from openai import OpenAI
+import openai
 from config.settings import OPENAI_API_KEY
 
 
-def generate_response_gpt3(prompt):
-    # Implement your GPT-3 API call here
-    # Use the OPENAI_API_KEY from the config.settings module
-    pass
+def generate_response_gpt3(message_list):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                     {"role": "system",
+                      "content": "You are an AI named sonic and you are in a conversation with a human. You can answer "
+                                 "questions, provide information, and help with a wide variety of tasks."},
+                     {"role": "user", "content": "Who are you?"},
+                     {"role": "assistant",
+                      "content": "I am the sonic powered by ChatGpt.Contact me sonic@deadlyai.com"},
+                 ] + message_list
+    )
+
+    return response["choices"][0]["message"]["content"].strip()
+
+
+def generate_code_response(message_text):
+    response = openai.Completion.create(
+        model="code-davinci-003",
+        prompt=message_text,
+        temperature=0,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    return response["choices"][0]["text"].strip()
